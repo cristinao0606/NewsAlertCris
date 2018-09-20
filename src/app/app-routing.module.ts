3693a5503f0dd185087848d2ea5d9930177de825
Marian Oprea <MarianO@office.fasma.ro>
2018-09-20 17:35:17 +0300

    tslint app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,26 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import { NewsListComponent } from "./newsPressMonitoring/news-list/news-list.component";
-import { NewsDetailsComponent } from "./newsPressMonitoring/news-details/news-details.component";
+import { NewsListComponent } from './newsPressMonitoring/news-list/news-list.component';
+import { NewsDetailsComponent } from './newsPressMonitoring/news-details/news-details.component';
 import { NewsMonitoringListComponent } from './newsPressMonitoring/news-monitoring-list/news-monitoring-list.component';
 
 
 
-const appRoutes:Routes=[
+const appRoutes: Routes = [
 
-    {path:'',redirectTo: '/news-list', pathMatch:'full'},
-    {path:'news-list',component:NewsListComponent, children:[
-        {path:'',component:NewsListComponent},
-        {path:'title',component:NewsDetailsComponent},
-     
+    {path: '', redirectTo: '/news-list', pathMatch: 'full'},
+    {path: 'news-list', component: NewsListComponent, children: [
+        {path: '', component: NewsListComponent},
+        {path: 'title', component: NewsDetailsComponent},
+
     ]},
-    {path:'newsmonitoring-list',component:NewsMonitoringListComponent},
-   
-]
+    {path: 'newsmonitoring-list', component: NewsMonitoringListComponent},
+
+];
 @NgModule({
-imports:[RouterModule.forRoot(appRoutes)],
-exports:[RouterModule]
+imports: [RouterModule.forRoot(appRoutes)],
+exports: [RouterModule]
 })
-export class AppRoutingModule
-{
+export class AppRoutingModule {
 
 }
